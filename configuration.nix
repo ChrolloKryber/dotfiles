2a7ba8a6f3d85e1f796eb1c74286047f8b08e3f9
ChrolloKryber <rishayraghav00@gmail.com>
2024-09-23 21:35:17 +0530

    "Updated themes and firefox settings"
    Signed-off-by: ChrolloKryber <rishayraghav00@gmail.com>

--- configuration.nix
+++ configuration.nix
@@ -123,19 +123,16 @@
   # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.raghav = {
+  users.users.archer = {
     isNormalUser = true;
-    description = "Rishay Raghav";
+    description = "Archer";
     extraGroups = [ "networkmanager" "wheel" "adm" "docker" ];
     shell = pkgs.zsh;
     packages = with pkgs; [
-      kdePackages.kate
-    #  thunderbird
+      vlc
     ];
   };
 
-  # Install firefox.
-  programs.firefox.enable = true;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -145,7 +142,6 @@
   environment.systemPackages = with pkgs; [
     blueberry
     brightnessctl
-    catppuccin-kvantum
     cliphist
     fastfetch
     git
@@ -162,7 +158,6 @@
     micro
     neovim
     networkmanagerapplet
-    nwg-look
     obsidian
     pipes
     playerctl
@@ -179,11 +174,11 @@
     swaynotificationcenter
     swww
     telegram-desktop
+    themechanger
     usbutils
     vlc
     vscode
     waybar
-    wallust
     wl-clipboard
     wlogout
     wmctrl
@@ -274,7 +269,6 @@
       sudo="sudo ";     
     };
     interactiveShellInit = ''
-      cat $HOME/.cache/wallust/sequences
       unalias -a
       eval "$(${pkgs.starship}/bin/starship init zsh)"
     '';
@@ -336,6 +330,7 @@
 
   # Firefox settings about:config
   programs.firefox = {
+    enable = true;
   	preferences = {
   		"browser.urlbar.shortcuts.bookmarks" = false;
   		"browser.urlbar.shortcuts.history" = false;
@@ -350,6 +345,73 @@
   		"fission.autostart" = false;
   		"gfx.webrender.all" = false;
   	};
+
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+      DisablePocket = true;
+
+      ExtensionSettings = {
+        "*" = {
+	  installation_mode = "force_installed";
+	  allowed_types = ["extension" "theme"];
+	};
+        "uBlock0@raymondhill.net" = {
+	  install_url="https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+	  installation_mode = "force_installed";
+	};
+        "addon@darkreader.org" = { 
+	  install_url="https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+ 	  installation_mode = "force_installed";
+	};
+	"78272b6fa58f4a1abaac99321d503a20@proton.me" = {
+	  install_url = "https://addons.mozilla.org/firefox/downloads/latest/proton-pass/latest.xpi";
+ 	  installation_mode = "force_installed";
+	};
+
+        "jid1-MnnxcxisBPnSXQ@jetpack" = {
+	  install_url="https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+	  installation_mode = "force_installed";
+	};
+
+       "sponsorBlocker@ajay.app" = {
+         install_url="https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
+ 	  installation_mode = "force_installed";
+	};
+
+       "jid1-BoFifL9Vbdl2zQ@jetpack"= {
+          install_url="https://addons.mozilla.org/firefox/downloads/latest/decentraleyes/latest.xpi";
+ 	  installation_mode = "force_installed";
+	};
+
+       "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = {
+         install_url="https://addons.mozilla.org/firefox/downloads/latest/return-youtube-dislikes/latest.xpi";
+	 installation_mode = "force_installed";
+	};
+
+       "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}" = { 
+         install_url="https://addons.mozilla.org/firefox/downloads/latest/styl-us/latest.xpi";
+ 	  installation_mode = "force_installed";
+	};
+
+       "{74145f27-f039-47ce-a470-a662b129930a}" = {
+         install_url="https://addons.mozilla.org/firefox/downloads/latest/clearurls/latest.xpi";
+   	 installation_mode = "force_installed";
+	};
+"{22b0eca1-8c02-4c0d-a5d7-6604ddd9836e}" = { 
+install_url="https://addons.mozilla.org/firefox/downloads/latest/nicothin-space/latest.xpi";
+ 	  installation_mode = "force_installed";
+	};
+
+    };
+};
+    
   };
 
   # Sudo Config
