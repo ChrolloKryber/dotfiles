2a70dfa41b9b88e63bb1b045e02325e193fcfcc3
ChrolloKryber <rishayraghav00@gmail.com>
2024-09-27 22:49:51 +0530

    Added more stuff
    Signed-off-by: ChrolloKryber <rishayraghav00@gmail.com>

--- .config/hypr/scripts/saveScreenshot.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-location=~/Pictures/Screenshots/Screenshot_$(date +%Y%m%d-%H%M%S).png
-grimblast save area $location
-notify-send -a "Grimblast" "Screenshot saved" -t 5000 -u normal 
-
-
