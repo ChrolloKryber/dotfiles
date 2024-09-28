117f16afebea0632b66decb94a17cd636d8aef7e
ChrolloKryber <rishayraghav00@gmail.com>
2024-09-28 21:42:37 +0530

    fuck chrome
    Signed-off-by: ChrolloKryber <rishayraghav00@gmail.com>

--- nixos/home.nix
+++ nixos/home.nix
@@ -36,11 +36,32 @@
     associations.added = {
       "image/jpeg"= ["org.gnome.Loupe.desktop"];
       "image/png"= ["org.gnome.Loupe.desktop"];
+      "x-scheme-handler/http"=["firefox.desktop"];
+      "x-scheme-handler/https"=["firefox.desktop"];
+      "x-scheme-handler/chrome"=["firefox.desktop"];
+      "text/html"=["firefox.desktop"];
+      "application/x-extension-htm"= ["firefox.desktop"];
+      "application/x-extension-html"= ["firefox.desktop"];
+      "application/x-extension-shtml"= ["firefox.desktop"];
+      "application/xhtml+xml"= ["firefox.desktop"];
+      "application/x-extension-xhtml"= ["firefox.desktop"];
+      "application/x-extension-xht"= ["firefox.desktop"];
     };
 
     defaultApplications = {
       "image/jpeg"= ["org.gnome.Loupe.desktop"];
-      "image/png"= ["org.gnome.Loupe.desktop"];      
+      "image/png"= ["org.gnome.Loupe.desktop"];
+      "x-scheme-handler/http"=["firefox.desktop"];
+      "x-scheme-handler/https"=["firefox.desktop"];
+      "x-scheme-handler/chrome"=["firefox.desktop"];
+      "text/html"=["firefox.desktop"];
+      "application/x-extension-htm"= ["firefox.desktop"];
+      "application/x-extension-html"= ["firefox.desktop"];
+      "application/x-extension-shtml"= ["firefox.desktop"];
+      "application/xhtml+xml"= ["firefox.desktop"];
+      "application/x-extension-xhtml"= ["firefox.desktop"];
+      "application/x-extension-xht"= ["firefox.desktop"];
+ 
     };
   };
 
