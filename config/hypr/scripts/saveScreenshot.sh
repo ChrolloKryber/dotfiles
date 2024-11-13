50e5ce8b67e3675ff6c1237c683f572c68d1be69
ChrolloKryber <rishay.raghav00@gmail.com>
2024-11-13 17:46:24 +0530

    we maining colemak now
    Signed-off-by: ChrolloKryber <rishay.raghav00@gmail.com>

--- config/hypr/scripts/saveScreenshot.sh
+++ config/hypr/scripts/saveScreenshot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 location=~/Pictures/Screenshots/Screenshot_$(date +%Y%m%d-%H%M%S).png
-grimblast save area $location
+grimblast save area "${location}" && swappy -f "${location}"
 notify-send -a "Grimblast" "Screenshot saved" -t 5000 -u normal 
 
 
