9cca8c7d904d2732ef2828884bc8dcbe3d6b22bf
ChrolloKryber <rishay.raghav00@gmail.com>
2025-01-28 23:38:14 +0530

    chore(update dotfiles): been some time since updated
    Signed-off-by: ChrolloKryber <rishay.raghav00@gmail.com>

--- config/hypr/scripts/saveScreenshot.sh
+++ config/hypr/scripts/saveScreenshot.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 location=~/Pictures/Screenshots/Screenshot_$(date +%Y%m%d-%H%M%S).png
-grimblast save area "${location}" && swappy -f "${location}" && notify-send -a "Grimblast" "Screenshot saved" -t 5000 -u normal 
+grimblast save area "${location}" && swappy -f "${location}" && notify-send -a "Grimblast" "Screenshot saved" -t 2000 -u low -r 85
